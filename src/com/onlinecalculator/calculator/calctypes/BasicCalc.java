a6c4d2cc7949eb8d4e5999acab54d6f68c153361
istar <istar@192.168.100.27>
2017-05-20 18:39:40 +0530

    Updated on 20/05/17 by shyamalasrikanth...

--- src/com/onlinecalculator/calculator/calctypes/BasicCalc.java
+++ src/com/onlinecalculator/calculator/calctypes/BasicCalc.java
@@ -6,17 +6,31 @@ public class BasicCalc
 	
 	public BasicCalc()
         {
+		int a=0;
+		int b=0;
+		int c=0;
+		res=0;
+		
 		
 	}
 	
 	public int menu()
         {
+        System.out.println("######select choice##########");
+    	System.out.println("1.addition");
+    	System.out.println("2.subtraction");
+    	System.out.println("3.multiplecation");
+    	System.out.println("4.division");
+    	Scanner s=new Scanner(System.in);
+    	ch=s.nextInt();
+    	return 0
+    	
     	
 	}
 	
     public void userInputs()
     {
-    	
+    Scanner s=new Scanner(System.in);	
     	
     }
 
