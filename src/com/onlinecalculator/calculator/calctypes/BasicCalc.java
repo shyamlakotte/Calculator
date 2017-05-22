966edd21fd259a221c2a1b7bc5cc516c1650cc2f
HOME <HOME@192.168.0.7>
2017-05-22 11:24:22 -0700

    program updated shyamalasrikanth JSP2 warangal

--- src/com/onlinecalculator/calculator/calctypes/BasicCalc.java
+++ src/com/onlinecalculator/calculator/calctypes/BasicCalc.java
@@ -3,61 +3,102 @@ import java.util.Scanner;
 
 public class BasicCalc
 {
+	int a,b,c,ch,res;
 	
 	public BasicCalc()
         {
-		int a=0;
-		int b=0;
-		int c=0;
+		 a=0;
+		 b=0;
+		 c=0;
+		ch=0;
 		res=0;
-		
-		
+	
 	}
 	
 	public int menu()
         {
-        System.out.println("######select choice##########");
     	System.out.println("1.addition");
     	System.out.println("2.subtraction");
-    	System.out.println("3.multiplecation");
-    	System.out.println("4.division");
-    	Scanner s=new Scanner(System.in);
-    	ch=s.nextInt();
-    	return 0
+    	System.out.println("3.mul");
+    	System.out.println("4.div");
+    	System.out.println("enter your choice");
+    	Scanner sc=new Scanner(System.in);
+    	ch=sc.nextInt();
+    	return 0;
     	
     	
 	}
 	
     public void userInputs()
     {
-    Scanner s=new Scanner(System.in);	
-    	
+    Scanner s=new Scanner(System.in);
+    System.out.println("enter first number");
+    a=s.nextInt();
+    System.out.println("enter second number");
+    b=s.nextInt();
     }
-
     public void performOperation()
 {
-    	
-        }
-       
+    	switch(ch)
+    	{
+    	case 1:
+    		System.out.println("addition");	    
+    		res=add();
+    		System.out.println("res"+res);
+    		break;
+    		
+    		
+    	case 2:
+    		System.out.println("subtraction");	    
+    		res=subtract();
+    		System.out.println("res"+res);
+    		break;
+    		
+    	case 3:
+    		System.out.println("multiplication");	    
+    		res=multiply();
+    		System.out.println("res"+res);
+    		break;
+    		
+    	case 4:
+    		System.out.println("division");	    
+    		res=divide();
+    		System.out.println("res"+res);
+    		break;
+    		
+    	}
     }
     
 	int add()
 	{
+		c=a+b;
+		return c;
 		
 	}
 	
 	int subtract()
 	{
-		
+		c=a-b;
+		return c;
 	}
 	
 	int multiply()
 	{
-		
+		c=a*b;
+		return c;
 	}
 	
 	int divide()
 	{
+		c=a/b;
+		return c;
+		
 	}
 	
 }
+
+
+
+
+
+
